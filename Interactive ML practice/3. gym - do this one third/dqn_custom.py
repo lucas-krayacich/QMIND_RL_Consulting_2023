13a4f10da0e6384b83320c3e14e0c0427b917ae8
Lucas Krayacich (19LMK2) <19lmk2@queensu.ca>
2024-02-07 16:09:49 -0500

    Interactive and DQN boilerplates.

---
+++ Interactive ML practice/3. gym - do this one third/dqn_custom.py
@@ -0,0 +1,34 @@
+import gymnasium as gym
+import random 
+import time
+
+from stable_baselines3 import DQN
+from IPython.display import clear_output
+
+
+env = gym.make("ALE/SpaceInvaders-v5", render_mode = "human")
+height, width, channels = env.observation_space.shape
+actions = env.action_space.n
+env.reset()
+
+# model = DQN("MlpPolicy", env, buffer_size=10000, verbose=1)
+# model.learn(total_timesteps=10)
+
+####### Now instead of implementing the built in library DQN we are going to build our own #######################
+
+# what is this going to consist of: 
+
+
+
+for steps in range(200): 
+    obs, info = env.reset()
+    done = False
+    
+    while not done: 
+        action, _states = model.predict(obs)
+        obs, reward, terminated, truncated, info = env.step(action)
+        if terminated or truncated: 
+            obs, info = env.reset()
+            print("Just died on episode ", steps)
+
+env.close()
